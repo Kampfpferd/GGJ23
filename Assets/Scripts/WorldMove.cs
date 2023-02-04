cd789ee6a84d17e954f369d86b0398082c357c18
Kampfpferd <123498629+Kampfpferd@users.noreply.github.com>
2023-02-04 09:59:04 +0100

    PlanetMove

--- Assets/Scripts/WorldMove.cs
+++ Assets/Scripts/WorldMove.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class WorldMove : MonoBehaviour
 {
+    Vector3 rotation;
+    public float movespeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotation = Vector3.back;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotation = Vector3.forward;
+        }
+        else
+        {
+            rotation = Vector3.zero;
+        }
+
+        transform.Rotate(rotation * movespeed * Time.deltaTime);
     }
 }
